8ab9e24e4a24f464a6ad25ae9f96eac103b3dbb6
Hriday Algh <hridayalgh@gmail.com>
2021-11-15 10:07:39 -0500

    bad solution

--- CCC '02/S3.cpp
+++ CCC '02/S3.cpp
@@ -1,68 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int r, c; cin >> r >> c;
-    bool yard[r][c];
+int main () {
+    int r; int c; cin >> r >> c;
+    char board[r][c];
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
-            char obs; cin >> obs;
-            if (obs == 'X') yard[i][j] = false;
-            else if (obs == '.') yard[i][j] = true;
+            cin >> board[i][j];
         }
     }
-    int m; cin >> m;
-    char moves[m+1];
-    for (int i = 0; i < m; i ++) {
+    int nmoves; cin >> nmoves;
+    char moves[nmoves+1];
+    for (int i = 0; i < nmoves; i++) {
         cin >> moves[i];
     }
-    moves[m] = 'D';
-
-    vector<vector<int>> finalpos = {};
-    for (int i = 0; i < r; i ++) {
-        for (int j = 0; j < c; j++) {
+    moves[nmoves] = 'Q';
+    
+    for (int row = 0; row < r; row++) {
+        for (int col = 0; col < c; col++) {
+            if (board[row][col] == 'X') continue;
             char facing[] = {'U', 'D', 'L', 'R'};
-            if(!yard[i][j]) continue;
-            for (char face : facing) {
-                int col = j;
-                int row = i;
+            for (int face = 0; face < 4; face++) {
+                int posX = col;
+                int posY = row;
                 for (auto move : moves) {
-                    cout << move << '\n';
-                    if (row >= r or row < 0 or col >= c or col < 0) break;
-                    if (!yard[row][col]) break;
                     if (move == 'F') {
-                        if (face == 'U') row -= 1;
-                        if (face == 'D') row += 1;
-                        if (face == 'L') col -= 1;
-                        if (face == 'R') col += 1;
-                    } else if (move == 'L') {
-                        if (face == 'U') face = 'L';
-                        if (face == 'D') face = 'R';
-                        if (face == 'L') face = 'U';
-                        if (face == 'R') face = 'D';
-                    } else if (move == 'R') {
-                        if (face == 'U') face = 'R';
-                        if (face == 'D') face = 'L';
-                        if (face == 'L') face = 'D';
-                        if (face == 'R') face = 'U';
+                        if (facing[face] == 'U') posY--;
+                        if (facing[face] == 'D') posY++;
+                        if (facing[face] == 'L') posX--;
+                        if (facing[face] == 'R') posX++;
                     }
-                    else if (move == 'D'){
-                        vector<int> bruhment = {row, col};
-                        finalpos.push_back(bruhment);
+                    else if (move == 'R') {
+                        if (facing[face] == 'D') facing[face] = 'L';
+                        else if (facing[face] == 'L') facing[face] = 'U';
+                        else if (facing[face] == 'U') facing[face] = 'R';
+                        else if (facing[face] == 'R') facing[face] = 'D';
+                    }
+                    else if (move == 'L') {
+                        if (facing[face] == 'D') facing[face] = 'R';
+                        else if (facing[face] == 'L') facing[face] = 'D';
+                        else if (facing[face] == 'U') facing[face] = 'L';
+                        else if (facing[face] == 'R') facing[face] = 'U';
+                    }
+                    if (posY >= r or posX < 0 or posX >= c or posY < 0) {
+                        break;
+                    }
+                    if (board[posY][posX] == 'X') break;
+                    if (move == 'Q') {
+                        if (board[posY][posX] == '.') board[posY][posX] = '*';
                     }
                 }
             }
         }
     }
-    for (auto i : finalpos) cout << i[0] << i[1] << '\n';
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            vector<int> bruhment = {i, j};
-            if (find(finalpos.begin(), finalpos.end(), bruhment) != finalpos.end()) {
-                cout << '*';
-            } else if (yard[i][j]) cout << '.';
-            else cout << 'X';
-        }
-        cout <<'\n';
+    for (int i = 0; i < r; i ++) {
+        for (int j = 0; j < c; j++) cout << board[i][j];
+        cout << '\n';
     }
 }
